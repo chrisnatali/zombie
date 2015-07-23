66c03e2133a6b41b5bea5494a61a0abab045fc19
chrisnatali <chris.natali@gmail.com>
2015-07-22 20:53:34 -0400

    submitted zombie code

--- poc_zombie.py
+++ poc_zombie.py
@@ -23,8 +23,8 @@ class Apocalypse(poc_grid.Grid):
     obstacles
     """
 
-    def __init__(self, grid_height, grid_width, obstacle_list = None, 
-                 zombie_list = None, human_list = None):
+    def __init__(self, grid_height, grid_width, obstacle_list=None, 
+                 zombie_list=None, human_list=None):
         """
         Create a simulation of given size with given obstacles,
         humans, and zombies
@@ -101,7 +101,7 @@ class Apocalypse(poc_grid.Grid):
         initial_val = self.get_grid_height() * self.get_grid_width()
         distance_field =\
             [[initial_val for dummy_row in range(self.get_grid_width())]
-            for dummy_col in range(self.get_grid_height())]
+             for dummy_col in range(self.get_grid_height())]
  
         boundary = poc_queue.Queue()
         if entity_type == HUMAN:
@@ -120,27 +120,77 @@ class Apocalypse(poc_grid.Grid):
                     visited.four_neighbors(cur_row, cur_col):
                 if visited.is_empty(neigh_row, neigh_col):
                     visited.set_full(neigh_row, neigh_col)
-                    distance_field[neigh_row, neigh_col] =\
-                    distance_field[cur_row, cur_col] + 1
-                    boundary.enqueue((neigh_row, neigh_col))
+                    # only update neighbor distance if its not an obstacle
+                    if self.is_empty(neigh_row, neigh_col):
+                        distance_field[neigh_row][neigh_col] =\
+                        distance_field[cur_row][cur_col] + 1
+                        boundary.enqueue((neigh_row, neigh_col))
 
         return distance_field
     
+
+    def _dists_for_neighs(self, distance_field, neighbors):
+        """
+        Get the distances from the distance field for each of the
+        (row, col) tuples in neighbors
+        """
+        return (distance_field[row][col] for row, col in neighbors)
+
+
+    def _find_move(self, distance_field, neighbors, towards=False):
+        """
+        find next cell to move to based on the neighbors, the distances
+        which drive the move and whether to move toward or away
+
+        Returns None if no possible moves
+        """
+        possible = [neigh for neigh in neighbors if self.is_empty(*neigh)]
+        if len(possible) > 0:
+            dists = self._dists_for_neighs(distance_field, possible)
+            enum_dists = sorted(enumerate(dists), 
+                                key=lambda item: item[1], 
+                                reverse=(not towards))
+            return possible[enum_dists[0][0]]
+
+        else:
+            return None
+
+
     def move_humans(self, zombie_distance_field):
         """
         Function that moves humans away from zombies, diagonal moves
         are allowed
         """
-        pass
+        for ind in range(len(self._human_list)):
+            human = self._human_list[ind]
+            neighbors = self.eight_neighbors(*human)
+            # we can stay put
+            neighbors.append(human)
+            move_to = self._find_move(zombie_distance_field,
+                                      neighbors,
+                                      towards=False)
+            if move_to is not None:
+                self._human_list[ind] = move_to
+
     
     def move_zombies(self, human_distance_field):
         """
         Function that moves zombies towards humans, no diagonal moves
         are allowed
         """
-        pass
+        for ind in range(len(self._zombie_list)):
+            zombie = self._zombie_list[ind]
+            neighbors = self.four_neighbors(*zombie)
+            # we can stay put
+            neighbors.append(zombie)
+            move_to = self._find_move(human_distance_field,
+                                      neighbors,
+                                      towards=True)
+            if move_to is not None:
+                self._zombie_list[ind] = move_to
+
 
 # Start up gui for simulation - You will need to write some code above
 # before this will work without errors
 
-poc_zombie_gui.run_gui(Apocalypse(30, 40))
+# poc_zombie_gui.run_gui(Apocalypse(30, 40))
